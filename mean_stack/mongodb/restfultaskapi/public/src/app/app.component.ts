c2134b3fe313c815a197e6064ffba566df68df27
Toby Kutler <tobykutler@gmail.com>
2019-07-17 18:04:42 -0700

    added a html display for data

--- mean_stack/mongodb/restfultaskapi/public/src/app/app.component.ts
+++ mean_stack/mongodb/restfultaskapi/public/src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+  task: any; 
+  alltask: any;
   constructor(private _httpService: HttpService){}
+  ngOnInit(){
+    this.getTaskFromService();
+    this.getalltasks()
+  }
+  getTaskFromService(){
+    let tempObservable = this._httpService.getonetask();
+    tempObservable.subscribe(data => {
+      console.log('got the task', data);
+      this.task = data['data'];
+      console.log(this.task);
+   });
+  }
+  getalltasks(){
+    let tempObservable3 = this._httpService.getTasks();
+    tempObservable3.subscribe(data =>{
+      console.log('got all tasks', data);
+      this.alltask = data['data'];
+      console.log(this.alltask)
+
+    });
+  }
 }
+
 
 
--- mean_stack/mongodb/restfultaskapi/public/src/app/http.service.ts
+++ mean_stack/mongodb/restfultaskapi/public/src/app/http.service.ts
@@ -5,24 +5,22 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class HttpService {
+  
   constructor(private _http: HttpClient){
-     this.getTasks();
-     this.getonetask();
-     this.postTasks();
+   //   this.getTasks();
+   //   this.getonetask();
+   //   this.postTasks();
    }
    getonetask(){
-    let tempObservable = this._http.get('/tasks/5d2d2f039079d71ce52b1174');
-    tempObservable.subscribe(data => console.log('got the task', data));
+    return this._http.get('/tasks/5d2fbdcb990784658aab3141');
    }
-   postTasks(){
-    let tempObservable = this._http.post('/tasks');
-    tempObservable.subscribe(data => console.log("posted task!", data));
-   }
+   // postTasks(){
+   //  let tempObservable2 = this._http.post('/tasks');
+   //  tempObservable2.subscribe(data => console.log("posted task!", data));
+   // }
    getTasks(){
-   // our http response is an Observable, store it in a variable
-      let tempObservable = this._http.get('/tasks');
-   // subscribe to the Observable and provide the code we would like to do with our data from the response
-      tempObservable.subscribe(data => console.log("Got our tasks!", data));
+      return this._http.get('/tasks');
+
    }
 }
 
