e3da4e898c929bac1784a6e534d07f194ebe5599
Toby Kutler <tobykutler@gmail.com>
2019-08-05 11:58:47 -0700

    first java program

---
+++ java_stack/fundamentals/Firstjava/Firstjava.java
@@ -0,0 +1,9 @@
+public class Firstjava {
+   public static void main (String [] args){
+    System.out.println("Hello my name is Toby");
+    System.out.println("I am 27 years old");
+    System.out.println("My hometown is Chevy Chase ");
+
+   }
+
+}
