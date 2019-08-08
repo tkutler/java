58d574cb39ba364fdb91667190d9cfafeb26075a
Toby Kutler <tobykutler@gmail.com>
2019-08-08 13:21:46 -0700

    oop game

---
+++ java_stack/springtoolsuite4/ObjectMaster/src/Human.java
@@ -0,0 +1,14 @@
+
+public class Human {
+	protected int health = 100;
+	protected int stealth = 3;
+	protected int strength = 3;
+	protected int intelligence = 3 ; 
+	
+	public void attack(Human human) {
+		human.health -= this.strength;
+		System.out.println ("attack was made");
+		
+	}
+
+}
---
+++ java_stack/springtoolsuite4/ObjectMaster/src/HumanTest.java
@@ -0,0 +1,9 @@
+
+public class HumanTest {
+	public static void main(String[] args) {
+		Human John = new Human ();
+		Human Pete = new Human();
+		John.attack(Pete);
+	}
+
+}
