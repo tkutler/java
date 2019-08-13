19b1d98c3d25dc8ac8f287be7d2300b4f476faa8
Toby Kutler <tobykutler@gmail.com>
2019-08-13 13:07:52 -0700

    first spring boot project

---
+++ java_stack/SpringPlatform/Strings/src/main/java/toby/StringsApplication.java
@@ -0,0 +1,25 @@
+package toby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringsApplication.class, args);
+	}
+	
+@RequestMapping("/")
+public String hello() { // 3
+    return "Hello client how are you doing?";
+}
+@RequestMapping("/random")
+public String rand() { // 3
+    return "Spring Boot is great! So easy to just respond with strings!?";
+}
+
+}
